137c5b162a784f9ba17b431078c6b626f77622a2
bay3s <sirbay3s@gmail.com>
2023-06-06 13:39:00 +0200

    scaling update

--- autometa/envs/walker/walker_dynamics_env.py
+++ autometa/envs/walker/walker_dynamics_env.py
@@ -11,7 +11,7 @@ from autometa.randomization.randomization_bound import RandomizationBound
 
 class WalkerDynamicsEnv(BaseWalkerEnv, EzPickle):
 
-    SCALING_FACTOR = 2.0
+    SCALING_FACTOR = 3.0
 
     RANDOMIZABLE_PARAMETERS = [
         RandomizationParameter(
