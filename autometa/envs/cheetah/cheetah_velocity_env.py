bbd86252e569470501913d6aaeb0321fb01e9c89
bay3s <sirbay3s@gmail.com>
2023-06-10 14:49:58 +0200

    add starting velocity for cheetah

--- autometa/envs/cheetah/cheetah_velocity_env.py
+++ autometa/envs/cheetah/cheetah_velocity_env.py
@@ -23,8 +23,8 @@ class CheetahVelocityEnv(BaseCheetahEnv, EzPickle):
             ),
             upper_bound=RandomizationBound(
                 type=RandomizationBoundType.UPPER_BOUND,
-                value=0.0,
-                min_value=0.0,
+                value=0.1,
+                min_value=0.1,
                 max_value=3.0,
             ),
             delta=0.03,
