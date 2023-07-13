2f53a305d8b25421fc9eb69f34c579e46ce592f5
bay3s <sirbay3s@gmail.com>
2023-07-13 20:11:45 +0200

    update point-nav auto dr config

--- runs/shell/auto_dr/auto_dr_point_navigation.sh
+++ runs/shell/auto_dr/auto_dr_point_navigation.sh
@@ -5,9 +5,9 @@
 #SBATCH --cpus-per-task=25
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
-#SBATCH --time=02:00:00
+#SBATCH --time=06:00:00
 
 source /home/${USER}/.bashrc
 source activate rl
 
-srun python3 $HOME/autometa/runs/run.py --algo=auto_dr --env=point_navigation --prod
+srun python3 $HOME/autometa/runs/run.py --algo=auto_dr --env=point_navigation
