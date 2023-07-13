06bd3f0e8ebc247f6798b99860c36b563d009ada
bay3s <sirbay3s@gmail.com>
2023-07-13 13:57:59 +0200

    update evaluation script

--- analysis/point_navigation/rl_squared_point_navigation.sh
+++ analysis/point_navigation/rl_squared_point_navigation.sh
@@ -5,9 +5,12 @@
 #SBATCH --cpus-per-task=25
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
-#SBATCH --time=06:00:00
+#SBATCH --time=10:00:00
 
-source /home/${USER}/.bashrc
-source activate rl
+#source /home/${USER}/.bashrc
+#source activate rl
 
-srun python3 $HOME/autometa/analysis/point_navigation/run_evaluation.py --algo=rl_squared --grid-size=3.0
+for seed in 2878 5069 5073 5250 4420
+  do
+    python3 $HOME/autometa/analysis/point_navigation/run_evaluation.py --algo=rl_squared --grid-size=3.0 --random-seed=$seed
+  done
